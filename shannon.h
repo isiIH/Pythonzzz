2f04ea3cad5a240f07dfe7a823b31f55a1ce612c
Isi <isidora.hernandez@alumnos.uach.cl>
2022-05-11 17:47:30 -0400

    coloqué la busqueda secuencial para mejorar el tiempo de ejecución

--- shannon.h
+++ shannon.h
@@ -8,114 +8,135 @@ using namespace std;
 struct Node {
     char simbolo;
     double probabilidad = 0.0;
-    string bitstream = ""; 
+    string bitstream = "";
 };
 
 void obtenerProb(vector<Node> &v, string texto);
-void encode(vector<Node> &v, int l, int r);
-void particion(vector<Node> v, int *l, int *r, double lprob, double rprob);
-void crearCodificado(vector<Node> v, string text);
-void decode(vector<Node> v, string text);
-void imprimirSimbolos(vector<Node> v);
+void asignaCodigos(vector<Node> &v, int l, int r);
+int particion(vector<Node> &v, int l, int r, double lprob, double rprob);
+void encode(vector<Node> &v, string text);
+void decode(vector<Node> &v, string text);
+void imprimirSimbolos(vector<Node> &v);
+int busquedaSecuencial(vector<Node> &v, int x);
 
 void obtenerProb(vector<Node> &v, string texto){
-    ifstream archivo;
-    archivo.open("archivos/" + texto, ios::in);
-    char letra;
     double total = 0.0;
+/*     vector<Node> f(256); */
+    int indice;
     Node s;
-    map<char,double> prob;
-    map<char, double>::iterator fi;
-    int i;
 
-    if(archivo.fail()){
-        cout << "Error" << endl;
-        exit(1);
-    }
-    while(archivo >> noskipws >> letra){
-        prob[letra]++;
-        total++;
-    }
-    archivo.close();
+    for(char c : texto){
+        if(int(c) < 256 && int(c)>=0){            
+            indice = busquedaSecuencial(v,c);
 
-    for(fi = prob.begin(), i = 0; fi != prob.end(); ++fi, ++i){
-        s.simbolo = (*fi).first;
-        s.probabilidad = (*fi).second / total;
-        v.push_back(s);
+            if(indice == -1){
+                s.simbolo = c;
+                s.probabilidad = 1;
+                v.push_back(s);
+            } else v[indice].probabilidad++;
+
+            total++;
+        }
     }
+
+    for(int i=0; i<v.size(); i++) {
+        v[i].probabilidad /= total;
+    }
+
+    /* for(char c : texto){
+        if(int(c) < 256 && int(c)>=0){
+            f[int(c)].simbolo = c;
+            f[int(c)].probabilidad++;
+            total++;
+        }
+    }
+
+    for(Node sim : f){
+        if(sim.probabilidad != 0){
+            sim.probabilidad /= total;
+            v.push_back(sim);
+        }
+    } */
 }
 
-void encode(vector<Node> &v, int l, int r) {
+
+void asignaCodigos(vector<Node> &v, int l, int r) {
     if(l<r){
-        int x, y;
-        x = l;
-        y = r;
-        particion(v, &l, &r, v[l].probabilidad, v[r].probabilidad);
-        for(int i=x; i<=l; i++){
-            v[i].bitstream += "1";
-            // cout << v[i].bitstream << endl;
+        int p = particion(v, l, r, v[l].probabilidad, v[r].probabilidad);
+        for(int i=l; i<=r; i++){
+            if(i<=p){
+                v[i].bitstream += "1";
+            } else {
+                v[i].bitstream += "0";
+            }
         }
-        for(int i=r; i<=y; i++){
-            v[i].bitstream += "0";
-            // cout << v[i].bitstream << endl;
-        }
-        // cout << "l: " << l << ", r: " << r << endl;
-        encode(v,x,l);
-        encode(v,r,y);
+        asignaCodigos(v,l,p);
+        asignaCodigos(v,p+1,r);
     }
 }
 
-void particion(vector<Node> v, int *l, int *r, double lprob, double rprob){
-    /* cout << *l << " " << *r << endl;
+int particion(vector<Node> &v, int l, int r, double lprob, double rprob){
+/*     cout << l << " " << r << endl;
     cout << "lprob: " << lprob << endl;
     cout << "rprob: " << rprob << endl; */
-    if(*l != *r-1){
+    if(l != r-1){
         if(lprob > rprob){
-            *r -= 1;
-            rprob += v[*r].probabilidad;
+            r -= 1;
+            rprob += v[r].probabilidad;
         } else {
-            *l += 1;
-            lprob += v[*l].probabilidad;
+            l += 1;
+            lprob += v[l].probabilidad;
         }
-        particion(v,l,r,lprob,rprob);
+        return particion(v, l, r, lprob, rprob);
     }
+    return l;
 }
 
-void crearCodificado(vector<Node> v, string text){
-    ifstream archivo;
+void encode(vector<Node> &v, string text){
     ofstream archivoMod;
-    archivo.open("archivos/" + text, ios::in);
     archivoMod.open("archivos/codificado.txt");
-    char letra;
+    int indice;
 
-    if(archivo.fail()){
-        cout << "Error" << endl;
-        exit(1);
+    for(char c : text){
+        indice = busquedaSecuencial(v,c);
+        archivoMod << v[indice].bitstream;
     }
+    archivoMod.close();
+    cout << "Se ha creado el archivo codificado" << endl;
+}
+
+void decode(vector<Node> &v, string texto){
+    ofstream archivoMod;
+    archivoMod.open("archivos/codificado.txt", ios::in);
+    char letra;
+    string codigo = "";
 
     map<char, string> CodeShannon;
     for(int i=0; i<v.size(); i++){
         CodeShannon[v[i].simbolo] = v[i].bitstream;
     }
 
-    while(archivo >> noskipws >> letra){
-        archivoMod << CodeShannon[letra];
+    for(char c : texto){
+        /*INSERTE CODIGO xd*/
+        archivoMod << "xd";
     }
-    cout << "Se ha creado el archivo codificado" << endl;
-    archivo.close();
     archivoMod.close();
 }
 
-void decode(vector<Node> v, string text){
-
-}
-
-void imprimirSimbolos(vector<Node> v){
+void imprimirSimbolos(vector<Node> &v){
     double suma = 0.0;
     for (int i = 0; i < v.size(); i++){
-        cout << v[i].simbolo << " " << v[i].probabilidad << " " << v[i].bitstream << endl;
+        cout << "'" << v[i].simbolo << "'\t" << v[i].probabilidad << "\t" << v[i].bitstream << endl;
         suma = suma + v[i].probabilidad;
     }
     cout << "suma: " << suma << endl; 
 }
 
+int busquedaSecuencial(vector<Node> &v, int x){
+	int i;
+	for(i=0; i<v.size(); i++){
+		if(x == v[i].simbolo)
+			return i;
+	}
+	return -1;
+}
--- trabajo.cpp
+++ trabajo.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <fstream>
-#include <string> 
+#include <string>
 #include <vector>
-#include <algorithm>
+#include <ctime>
 #include "huffman.h" 
 #include "shannon.h" 
 using namespace std;
@@ -11,18 +11,63 @@ void leerArchivo(string text);
 
 int main(int argc, char **argv){
 
-    string texto = "pr.txt";
-    vector<Node> v;
+    if(argc != 2){
+		cout << "Error. Debe ejecutarse como ./trabajo archivo" << endl;
+		exit(EXIT_FAILURE);
+	}
 
+    string file = argv[1];
+    vector<Node> v;
+    ifstream archivo;
+    archivo.open("archivos/" + file, ios::in);
+    string linea;
+    string texto = "";
+
+    if(archivo.fail()){
+        cout << "Error" << endl;
+        exit(1);
+    }
+    while(!archivo.eof()){
+        getline(archivo, linea);
+        texto += linea;
+    }
+    archivo.close();
+
+    clock_t start = clock();
     obtenerProb(v, texto);
+    clock_t end = clock();
+    cout << "Tiempo de ejecución obtenerProb: " << (double(end - start))/(CLOCKS_PER_SEC) << "s" << endl;
     sort(v.begin(), v.end(), [] (Node &x, Node &y) { return x.probabilidad > y.probabilidad; });
 
     int l = 0;
     int r = v.size() - 1;
-    encode(v, l, r);
+    start = clock();
+    asignaCodigos(v, l, r);
+    end = clock();
+    cout << "Tiempo de ejecución asignaCodigos: " << (double(end - start))/(CLOCKS_PER_SEC) << "s" << endl;
 
     imprimirSimbolos(v);
-    crearCodificado(v, texto);
+    start = clock();
+    encode(v, texto);
+    end = clock();
+    cout << "Tiempo de ejecución encode: " << (double(end - start))/(CLOCKS_PER_SEC) << "s" << endl;
 
+    /* leerArchivo(texto); */
 	return EXIT_SUCCESS;
+}
+
+void leerArchivo(string text) {
+    ifstream archivo;
+    archivo.open("archivos/" + text, ios::in);
+    string linea;
+
+    if(archivo.fail()){
+        cout << "Error" << endl;
+        exit(1);
+    }
+
+    while(getline(archivo, linea)){
+        cout << linea << endl;
+    }
+    archivo.close();
 }
