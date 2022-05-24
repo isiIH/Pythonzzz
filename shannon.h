c7255d400d46dd4f9e7171b71d92d5d8548c2bbd
Isi <isidora.hernandez@alumnos.uach.cl>
2022-05-24 18:00:09 -0400

    Agregué el código de shannon

--- shannon.h
+++ shannon.h
@@ -1,69 +1,253 @@
-
 #include <iostream>
-#include <fstream>
-#include <map>
-#include <bitset>
-#include <stdlib.h>
-
+#include <vector>
+#include <string>
 using namespace std;
 
-class shannonF{
-
-
-    struct Node
-	{
-		char symbol;
-		float probability;
-	};
-
-    int tsize;
-    Node *ptable;
-    map<char, string> bits;
-
-    public:
-        
-        void Encode(const char *text, const char *output){
-
-            char symbol;
-            float ftot;
-            int i, total = 0; 
-            ifstream inputData;
-            inputData.open(text);
-            if (inputData.is_open()){
-                map<char, int> frequencyMap;
-                while (inputData >> noskipws >> symbol){
-
-                    frequencyMap[symbol]++;
-                    total++;
-                }
-                //------------------------------------------------------------------------------------------------------
-                tsize = (int)frequencyMap.size(); // // cantidad de signos distintos en el texto
-                ptable = new Node[tsize];  // crea los nodos con la cantidad de signos que hay  en el texto
-                ftot = float(total);
-                map<char, int>::iterator fi;
-                for (fi = frequencyMap.begin(), i = 0; fi != frequencyMap.end(); ++fi, ++i)
-                {
-                    ptable[i].symbol = (*fi).first; // agrega el simbolo al node 
-                    ptable[i].probability = float((*fi).second) / ftot; //agrega con que frecuencia esta . 
-                
-
-                }
-
-                for (i = 0; i < tsize; i++)
-                {
-                    cout << ptable[i].symbol << " " << ptable[i].probability << endl;
-                }
-
-            
-
-
-            }else  
-                cout << "Error" <<  endl;
-
-        }
+struct Simbolo {
+    char letra;
+    double probabilidad = 1.0;
+    string bitstream = "";
 };
 
+struct PosCodificado {
+    char simbolo;
+    int posInicial;
+    int posFinal;
+};
+
+void obtenerProb(vector<Simbolo> &F, string texto);
+
+void asignaCodigos(vector<Simbolo> &F, int l, int r);
+int particion(vector<Simbolo> &F, int l, int r, double lprob, double rprob);
+
+string encode(vector<Simbolo> &F, string texto);
+string encode2(vector<Simbolo> &F, vector<PosCodificado> &P, string texto);
+
+string decode(vector<Simbolo> &F, string texto);
+string decode2(vector<Simbolo> &F, string texto, int k, int u);
+string decode3(vector<Simbolo> &F, vector<PosCodificado> &P, int i, int j, string texto);
+
+void imprimirSimbolos(vector<Simbolo> &F);
+void imprimirPosiciones(vector<PosCodificado> &P);
+
+int busquedaSecuencial(vector<Simbolo> &F, int x);
+
+void obtenerProb(vector<Simbolo> &F, string texto){
+    double total = 0.0;
+    int indice;
+    Simbolo s;
+
+    for(char c : texto){          
+        indice = busquedaSecuencial(F,c);
+
+        if(indice == -1){
+            s.letra = c;
+            F.push_back(s);
+        } else F[indice].probabilidad++;
+
+        total++;
+    }
+
+    for(int i=0; i<F.size(); i++) {
+        F[i].probabilidad /= total;
+    }
+}
 
 
+void asignaCodigos(vector<Simbolo> &F, int l, int r) {
+    if(l<r){
+        int p = particion(F, l, r, F[l].probabilidad, F[r].probabilidad);
+        for(int i=l; i<=r; i++){
+            if(i<=p){
+                F[i].bitstream += "1";
+            } else {
+                F[i].bitstream += "0";
+            }
+        }
+        asignaCodigos(F,l,p);
+        asignaCodigos(F,p+1,r);
+    }
+}
 
+int particion(vector<Simbolo> &F, int l, int r, double lprob, double rprob){
+/*     cout << l << " " << r << endl;
+    cout << "lprob: " << lprob << endl;
+    cout << "rprob: " << rprob << endl; */
+    if(l != r-1){
+        if(lprob > rprob){
+            r -= 1;
+            rprob += F[r].probabilidad;
+        } else {
+            l += 1;
+            lprob += F[l].probabilidad;
+        }
+        return particion(F, l, r, lprob, rprob);
+    }
+    return l;
+}
 
+string encode(vector<Simbolo> &F, string texto){
+    /* ofstream archivoMod;
+    archivoMod.open("archivos/codificado.txt"); */
+    int indice;
+    string textoCod = "";
+
+    for(char c : texto){
+        indice = busquedaSecuencial(F,c);
+        /* archivoMod << F[indice].bitstream; */
+        textoCod += F[indice].bitstream;
+    }
+
+    /* archivoMod.close();
+    cout << "Se ha creado el archivo codificado.txt" << endl; */
+    return textoCod;
+}
+
+string encode2(vector<Simbolo> &F, vector<PosCodificado> &P, string texto){
+    /* ofstream archivoMod;
+    archivoMod.open("archivos/codificado.txt"); */
+    int indice;
+    string textoCod = "";
+    PosCodificado p;
+
+    for(char c : texto){
+        p.simbolo = c;
+        p.posInicial = textoCod.length();
+
+        indice = busquedaSecuencial(F,c);
+        /* archivoMod << F[indice].bitstream; */
+        textoCod += F[indice].bitstream;
+
+        p.posFinal = textoCod.length()-1;
+        P.push_back(p);
+    }
+
+    /* archivoMod.close();
+    cout << "Se ha creado el archivo codificado.txt" << endl; */
+    return textoCod;
+}
+
+string decode(vector<Simbolo> &F, string texto){
+    /* ofstream archivoMod;
+    archivoMod.open("archivos/decodificado.txt"); */
+    int l = 0;
+    int i, r;
+    string textoDecod = "";
+
+    while(l+F[0].bitstream.length() <= texto.length()) {
+        i = 0;
+        r = F[i].bitstream.length();
+        /* cout << "l: " << l << " r: " << r << " " << texto.substr(l,r) << endl; */
+        while(texto.substr(l,r) != F[i].bitstream) {
+            /* cout << F[i].bitstream << endl; */
+            i += 1;
+            /* cout << i << endl; */
+            r = F[i].bitstream.length();
+        }
+
+        /* archivoMod << F[i].letra; */
+        textoDecod += F[i].letra;
+        l += r;
+    }
+    // cout << textoDecod << endl; 
+    /* archivoMod.close();
+    cout << "Se ha creado el archivo decodificado.txt" << endl; */
+
+    return textoDecod;
+}
+
+string decode2(vector<Simbolo> &F, string texto, int k, int u) {
+    if(k<0 || k>(u-1)) {
+        cout << "Posición k fuera de rango..." << endl;
+        return "";
+    }
+
+    /* ofstream archivoMod;
+    archivoMod.open("archivos/decodificado.txt"); */
+    int l = 0;
+    int i, r;
+    string textoDecod = "";
+
+    while(textoDecod.length()-1 !=k) {
+        i = 0;
+        r = F[i].bitstream.length();
+        /* cout << "l: " << l << " r: " << r << " " << texto.substr(l,r) << endl; */
+        while(texto.substr(l,r) != F[i].bitstream) {
+            /* cout << v[i].bitstream << endl; */
+            i += 1;
+            /* cout << i << endl; */
+            r = F[i].bitstream.length();
+        }
+
+        /* archivoMod << F[i].letra; */
+        textoDecod += F[i].letra;
+        l += r;
+    }
+    /* cout << textoDecod << endl; */
+    /* archivoMod.close();
+    cout << "Se ha creado el archivo decodificado.txt" << endl; */
+
+    return textoDecod;
+}
+
+string decode3(vector<Simbolo> &F, vector<PosCodificado> &P, int i, int j, string texto){
+    if(i<1 || i>j || j>P.size()-1) {
+        cout << "Posiciones i, j fuera de límite..." << endl;
+        return "";
+    }
+
+    /* ofstream archivoMod;
+    archivoMod.open("archivos/decodificado.txt"); */
+    texto = texto.substr(P[i].posInicial, P[j].posFinal - P[i].posInicial + 1);
+    /* cout << texto << endl; */
+
+    int l = 0;
+    int x, r;
+    string textoDecod = "";
+
+    while(l+F[0].bitstream.length() <= texto.length()) {
+        x = 0;
+        r = F[x].bitstream.length();
+        /* cout << "l: " << l << " r: " << r << " " << texto.substr(l,r) << endl; */
+        while(x < F.size() && texto.substr(l,r) != F[x].bitstream) {
+            /* cout << F[x].letra << " " << F[x].bitstream << endl; */
+            x += 1;
+            /* cout << x << endl; */
+            r = F[x].bitstream.length();
+        }
+
+        /* archivoMod << F[x].letra; */
+        textoDecod += F[x].letra;
+        l += r;
+    }
+
+    /* cout << textoDecod << endl;  */
+
+    /* archivoMod.close();
+    cout << "Se ha creado el archivo decodificado.txt" << endl; */
+    return textoDecod;
+}
+
+void imprimirSimbolos(vector<Simbolo> &F){
+    double suma = 0.0;
+    for (int i = 0; i < F.size(); i++){
+        cout << "'" << F[i].letra << "'\t" << F[i].probabilidad << " \t " << F[i].bitstream << endl;
+        suma = suma + F[i].probabilidad;
+    }
+    cout << "suma: " << suma << endl; 
+}
+
+void imprimirPosiciones(vector<PosCodificado> &P){
+    for (int i = 0; i < P.size(); i++){
+        cout << "'" << P[i].simbolo << "'\t" << P[i].posInicial << " " << P[i].posFinal << endl;
+    }
+}
+
+int busquedaSecuencial(vector<Simbolo> &F, int x){
+	int i;
+	for(i=0; i<F.size(); i++){
+		if(x == F[i].letra)
+			return i;
+	}
+	return -1;
+}
--- trabajo.cpp
+++ trabajo.cpp
@@ -1,24 +1,45 @@
 #include <iostream>
 #include <string> 
-#include <fstream> 
-#include "huffman.h" 
-#include "shannon.h" 
-#include <unordered_map> 
+#include <fstream>
+#include <ctime>
 #include <vector>
+#include <algorithm>
+#include "huffman.h"
+#include "shannon.h" 
 using namespace std;
 
 void readFile(string *text);
+void ejecutarShannon(string texto);
+void ejecutarHuffman();
 
 //--------------------------------------------------------------------------------------------
 
 int main(int argc, char **argv){
     
-   
-    string text = "" ;
-    readFile(&text);
-    
+    if(argc != 2){
+		cout << "Error. Debe ejecutarse como ./trabajo archivo" << endl;
+		exit(EXIT_FAILURE);
+	}
 
-    createArbol(&text);
+    string file = argv[1];
+    ifstream archivo;
+    archivo.open("archivos/" + file, ios::in);
+    string texto = "";
+    char letra;
+
+    if(archivo.fail()){
+        cout << "Error" << endl;
+        exit(1);
+    }
+    while(archivo >> noskipws >> letra) {
+        if(int(letra) < 256 && int(letra)>=0){
+            texto += letra;
+        }
+    }
+    archivo.close();
+
+    ejecutarShannon(texto);
+    /* ejecutarHuffman(); */
     
 	return EXIT_SUCCESS;
 }
@@ -62,7 +83,68 @@ void readFile(string *text){
     }
     
     //cout << *text << endl; 
- 
 
     
+}
+
+void ejecutarShannon(string texto) {
+    vector<Simbolo> F;
+    vector<PosCodificado> P;
+    string textoCod, textoDecod;
+    double total = 0.0;
+
+    clock_t start = clock();
+    obtenerProb(F, texto);
+    clock_t end = clock();
+    cout << "Tiempo de ejecución obtenerProb: " << (double(end - start))/(CLOCKS_PER_SEC) << "s" << endl;
+    total += (double(end - start))/(CLOCKS_PER_SEC);
+    sort(F.begin(), F.end(), [] (Simbolo &x, Simbolo &y) { return x.probabilidad > y.probabilidad; });
+
+    int l = 0;
+    int r = F.size() - 1;
+    start = clock();
+    asignaCodigos(F, l, r);
+    end = clock();
+    cout << "Tiempo de ejecución asignaCodigos: " << (double(end - start))/(CLOCKS_PER_SEC) << "s" << endl;
+    total += (double(end - start))/(CLOCKS_PER_SEC);
+
+    imprimirSimbolos(F);
+    
+    start = clock();
+    textoCod = encode(F, texto);
+    /* textoCod = encode2(F, P, texto); */
+    end = clock();
+    cout << "Tiempo de ejecución encode: " << (double(end - start))/(CLOCKS_PER_SEC) << "s" << endl;
+    total += (double(end - start))/(CLOCKS_PER_SEC);
+
+    start = clock();
+    textoDecod = decode(F, textoCod);
+    /* textoDecod = decode2(F, textoCod, 7, texto.size());
+    textoDecod = decode3(F, P, 1, 20, textoCod); */
+    end = clock();
+    cout << "Tiempo de ejecución decode: " << (double(end - start))/(CLOCKS_PER_SEC) << "s" << endl;
+    total += (double(end - start))/(CLOCKS_PER_SEC);
+
+    cout << "Tiempo total: " << total << "s" << endl;
+
+    /* imprimirPosiciones(P); */
+
+    cout << "\n" << "Tamaño Simbolo: " << sizeof(struct Simbolo) << " bytes" << endl;
+    cout << "Tamaño PosCodificado: " << sizeof(struct PosCodificado) << " bytes" << endl;
+    cout << "Tamaño F: " << (F.size() * double(sizeof(struct Simbolo)))/1048576 << " MB" << endl;
+    cout << "Tamaño P: " << (P.size() * double(sizeof(struct PosCodificado)))/1048576 << " MB" << endl;
+    cout << "Tamaño texto original: " << (double(texto.length()))/1048576 << " MB" << endl;
+    cout << "Tamaño texto codificado: " << ( (double(textoCod.length()))/1048576 )/8 << " MB" << endl;
+    cout << "Tamaño texto decodificado: " << (double(textoDecod.length()))/1048576 << " MB" << endl;
+    /* cout << texto << endl;
+    cout << textoCod << endl;
+    cout << textoDecod << endl; */
+}
+
+void ejecutarHuffman() {
+    string text = "" ;
+    readFile(&text);
+    
+
+    createArbol(&text);
 }
